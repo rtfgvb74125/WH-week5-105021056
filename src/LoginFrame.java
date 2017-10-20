f11ad9f027751f94b10b24d5d840c76ac98b2f9e
rtfgvb74125 <105021056@live.asia.edu.tw>
2017-10-20 14:38:42 +0800

    finish

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginFrame extends JFrame{
     private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
     private int frmW = 300, frmH = 150, screenW, screenH;
+    private Container cp;
+
+    private JLabel jlabID = new JLabel("ID");
+    private JLabel jlabPW = new JLabel("Password");
+    private JTextField jtxId = new JTextField();
+    private JPasswordField jPW = new JPasswordField();
+    private JButton jbtnExit = new JButton("Exit");
+    private JButton jbtnLogin = new JButton("Login");
+    private String  Name = "h304";
+    private String Password = "23323456";
     public LoginFrame(){
         init();
     }
@@ -12,5 +24,43 @@ public class LoginFrame extends JFrame{
         screenH = dim.height;
         this.setBounds(screenW/2-frmW/2,screenH/2-frmH/2,frmW,frmH);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        cp = this.getContentPane();
+        cp.setLayout(new GridLayout(3,2,1,1));
+        jlabID.setHorizontalAlignment(SwingConstants.RIGHT);
+        jlabPW.setHorizontalAlignment(SwingConstants.RIGHT);
+        cp.add(jlabID);
+        cp.add(jtxId);
+        cp.add(jlabPW);
+        cp.add(jPW);
+        cp.add(jbtnExit);
+        cp.add(jbtnLogin);
+
+        // control
+        jbtnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        jbtnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(jtxId.getText().equals(Name) && (new String(jPW.getPassword())).equals(Password)){
+                    MainFrame mf = new MainFrame(LoginFrame.this);
+                    mf.setVisible(true);
+                    LoginFrame.this.setVisible(false);
+                    jtxId.setText("");
+                    jPW.setText("");
+
+                }else{
+                    if(!jtxId.getText().equals(Name)){
+                        JOptionPane.showMessageDialog(LoginFrame.this,"your ID is wroong");
+                    }else if(!(new String(jPW.getPassword())).equals(Password)){
+                        JOptionPane.showMessageDialog(LoginFrame.this,"your password is wrong");
+                    }
+                }
+            }
+        });
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,75 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame {
+    private LoginFrame lgfm;
+    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private int frmW = 500, frmH = 400, screenW, screenH;
+    private JTextField jtx = new JTextField();
+    private JButton jbtn[] = new JButton[10];
+    private JButton jbtnClear = new JButton("Clear");
+    private JButton jbtnExit = new JButton("Go back");
+    private JPanel jpn = new JPanel(new GridLayout(4, 3, 1, 1));
+    private Container cp;
+
+    public MainFrame(LoginFrame lg) {
+        lgfm = lg;
+        init();
+    }
+
+    private void init() {
+        screenW = dim.width;
+        screenH = dim.height;
+        this.setBounds(screenW / 2 - frmW / 2, screenH / 2 - frmH / 2, frmW, frmH);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                lgfm.setVisible(true);
+                dispose();
+            }
+        });
+        cp = this.getContentPane();
+        cp.setLayout(new BorderLayout(3, 3));
+        cp.add(jtx, BorderLayout.NORTH);
+        cp.add(jpn, BorderLayout.CENTER);
+        jtx.setEditable(false);
+        for (int i = 0; i < 10; i++) {
+            jbtn[i] = new JButton();
+            jpn.add(jbtn[i]);
+            int x = new Random().nextInt(10);
+            jbtn[i].setText(Integer.toString(x));
+
+            //control
+            jbtn[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton tmp = (JButton) e.getSource();
+                    jtx.setText(jtx.getText() + tmp.getText());
+                }
+            });
+
+        }
+        jpn.add(jbtnExit);
+        jpn.add(jbtnClear);
+        jbtnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lgfm.setVisible(true);
+                dispose();
+            }
+        });
+        jbtnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jtx.setText(" ");
+            }
+        });
+    }
 }
+
+
